9a8a4a3ca4d423d6d6235f55968bdf462fd3d0af
Prakrit Duangsutha <prakrit_duangsutha@outlook.com>
2023-06-13 20:48:19 -0400

    Add 400 for Paccar

--- app.py
+++ app.py
@@ -60,3 +60,7 @@ def paccar_200():
 @app.route("/paccar-404/supplierti", methods=["GET", "POST"])
 def paccar_404():
     return ("Not Found", 404)
+
+@app.route("/paccar-400/supplierti", methods=["GET", "POST"])
+def paccar_404():
+    return ("Bad Request", 400)
